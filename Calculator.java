e2ba8bf12981fb51947b421d70f3ed28e824ad08
Eldar <desert73eagle@gmail.com>
2018-05-03 02:16:00 +0300

    Update Files

--- Calculator.java
+++ Calculator.java
@@ -1,62 +1,62 @@
 public class Calculator{
 	public static void main(String[] args) {
-		boolean a= 1+1==2;
-		boolean b= 30-5==25;
-		boolean c= 30*4==120;
-		boolean d= 50/25==2;
-		boolean f= 10%3==1;
-		boolean e= 185+145==330;
-		boolean	g= 48-15==33;
-		boolean h= 12*2==24;
-		boolean	k= 72/8==9;
-		boolean l= 20%9==2;
-		boolean m= square(5)==25;
-		boolean o=square1(7)==49;
+		boolean a = 1 + 1 == 2;
+		boolean b = 30 - 5 == 25;
+		boolean c = 30 * 4== 120;
+		boolean d = 50 / 25 == 2;
+		boolean f = 10 % 3 == 1;
+		boolean e = 185 + 145 == 330;
+		boolean	g = 48 - 15 == 33;
+		boolean h = 12 * 2 == 24;
+		boolean	k = 72 / 8 == 9;
+		boolean l = 20 % 9 == 2;
+		boolean m = square (5) == 25;
+		boolean o = square1 (7) == 49;
 
 		if (a) {
-			System.out.println("1+1=2");
+			System.out.println("1 + 1= 2");
 		} if (b) {
-			System.out.println("30-5=25");
+			System.out.println("30 - 5 = 25");
 		}
 		if (c) {
-			System.out.println("30*4=120");
+			System.out.println("30 * 4= 120 ");
 		}
 		if (d) {
-			System.out.println("50/25=2");
+			System.out.println("50 / 25 = 2 ");
 		}
 		if (f) {
-			System.out.println("10%3=0.3");
+			System.out.println("10 % 3 = 0.3 ");
 		}
 		if (e) {
-			System.out.println("185+145=330");
+			System.out.println("185 + 145 = 330 ");
 		} if (g) {
-			System.out.println("48-15=33");
+			System.out.println("48 - 15 = 33");
 		}
 		if (h) {
-			System.out.println("12*2=24;");
+			System.out.println("12 * 2 = 24");
 		}
 		if (k) {
-			System.out.println("72/8=9");
+			System.out.println("72 / 8 = 9");
 		}
 		if (l) {
-			System.out.println("20%9=1.8");
+			System.out.println("20 % 9 = 1.8");
 		}
 		if (m) {
-			System.out.println("5^2=25");
+			System.out.println("5 ^ 2 = 25");
 		}
 		if (o) {
-			System.out.println("7^2=49");
+			System.out.println("7 ^ 2 = 49");
 		}
 	}
-	static int square(int m){
-        int result1 = m*m;
+	static int square (int m){
+        int result1 = m * m;
        
         return result1;
        
     }  
-    static int square1(int o){
+    static int square1 (int o){
         
-        int result2 = o*o;
+        int result2 = o * o;
        
         return result2;
     }     
---
+++ MyFirstGame.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyFirstGame {
+    public static void main(String[] argc) {
+        int randomNumber;
+        int enteredNumber; 
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        
+        randomNumber = random.nextInt(100);
+
+        System.out.print("Enter num");
+        enteredNumber = scanner.nextInt();
+
+        if (enteredNumber > randomNumber) {
+            System.out.print("The number you entered is greater than what the computer intended");
+        } 
+        else if (enteredNumber < randomNumber) {
+            System.out.print("The number you entered is less than what the computer");
+        } 
+        else if(enteredNumber == randomNumber){
+            System.out.print("You guessed!");
+        }
+
+    }
+}
