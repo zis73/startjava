8e2ae89d98b5bba7696d56d726ad2a5bb5ad4d0c
Eldar <desert73eagle@gmail.com>
2018-05-04 00:12:45 +0300

    Update files

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -20,22 +20,6 @@ public class MyFirstGame {
             System.out.print("The number you entered is less than what the computer");
         } 
         else if(enteredNumber == randomNumber){
-
-        int randNum;
-        int enterNum; Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        
-        randNum = random.nextInt(100);
-
-        System.out.print("Enter num");
-        enterNum = scanner.nextInt();
-        if (enterNum > randNum) {
-            System.out.print("The number you entered is greater than what the computer intended");
-        } else if (enterNum < randNum) {
-            System.out.print("The number you entered is less than what the computer");
-        } else if(enterNum == randNum){
-            System.out.print("You guessed!");
         }
-
     }
 }
